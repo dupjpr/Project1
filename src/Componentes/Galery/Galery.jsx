e0f108cdc57a8a5309da5bc01b3fc3cd5b87a7af
Juan Pablo R <dupjprgmail.com>
2021-04-27 08:08:31 -0500

    the uuid library is added and setup in the galery component

--- src/Componentes/Galery/Galery.jsx
+++ src/Componentes/Galery/Galery.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { v4 as uuid } from 'uuid';
 
 const Galery = () => {
 
@@ -9,7 +10,7 @@ const Galery = () => {
 
     return (
         <section>
-            {storeData?.data &&  storeData.data.map((item)=> <div key={item.id}>
+            {storeData?.data &&  storeData.data.map((item)=> <div key={uuid()}>
                 {item.name}
             </div>)}
         </section>
